e37b330fada5a94b20de570d10ffbd2f8d159c89
rox <rox>
2010-01-13 05:04:24 +0000

    change puts to logger proc.
    overload read instead of using a refchan for stdin due to the problems replacing stdin on windows.
    
    consolidate namespace vars.
    
    add cmdline args.

--- httpd.tcl
+++ httpd.tcl
@@ -2,7 +2,7 @@ namespace eval httpd {}
 
 proc httpd::incoming_connection {s addr port} {
     variable incoming_req
-    #_puts "connection $s $addr $port"
+    #logger "connection $s $addr $port"
     fconfigure $s -blocking 0 -translation crlf
     fileevent $s readable [list [namespace current]::read_conn $s $addr]
     dict set incoming_req($s) body {}
@@ -18,7 +18,7 @@ proc httpd::timeout {s} {
 proc httpd::read_conn {s addr} {
     variable incoming_req
 
-    #_puts "read conn $s $addr"
+    #logger "read conn $s $addr"
     if {[eof $s]} {close $s; return}
     after cancel [list [namespace current]::timeout $s]
     after 10000 [list [namespace current]::timeout $s]
@@ -29,12 +29,12 @@ proc httpd::read_conn {s addr} {
         return
     } elseif {![dict exists $incoming_req($s) header]} {
         if {![regexp {^([A-Z]{3,4})\s+(.*)\s+HTTP/1\.[01]\n} [dict get $incoming_req($s) body] -> method uri]} {
-            httpd::http_error $s 500 "500 Internal Server Error" "Invalid request"
+            http_error $s 500 "500 Internal Server Error" "Invalid request"
         }
         set uri [string map [list "\\" "\\\\"] $uri]
         regsub -all -- {%([A-Fa-f0-9][A-Fa-f0-9])} $uri {\\u00\1} uri
         set uri [subst -novar -nocommand $uri]
-        _puts "$s $method $uri"
+        logger "$s $method $uri"
 
         dict set incoming_req($s) uri $uri
         dict set incoming_req($s) method $method
@@ -52,14 +52,14 @@ proc httpd::read_conn {s addr} {
 
     set tmp $incoming_req($s)
     unset incoming_req($s)
-    #_puts "request dict: $tmp"
+    #logger "request dict: $tmp"
 
     request $s $addr $tmp
 }
 
 proc httpd::set_env {s addr req} {
     global request
-    variable httpd
+    variable config
     unset -nocomplain request
     set request(REMOTE_ADDR) $addr
     if {[dict exists $req header host]} {
@@ -67,8 +67,8 @@ proc httpd::set_env {s addr req} {
     } else {
         set request(HTTP_HOST) [lindex [fconfigure $s -sockname] 0]
     }
-    set request(SERVER_PORT) $httpd(port)
-    set request(DOCUMENT_ROOT) "$::base_dir"
+    set request(SERVER_PORT) $config(port)
+    set request(DOCUMENT_ROOT) "$config(base_dir)"
     set request(PATH_INFO) [lindex [split [dict get $req uri] ?] 0]
     if {[string match */ $request(PATH_INFO)]} { append request(PATH_INFO) index.html }
     set request(REQUEST_URI) [dict get $req uri]
@@ -86,44 +86,45 @@ proc httpd::set_env {s addr req} {
         set request(CONTENT_TYPE) [dict get $req header content-type]
         set request(BODY) [dict get $req body]
     } else {
-        httpd::http_error $s 500 "500 Internal Server Error" "Invalid request method"
+        http_error $s 500 "500 Internal Server Error" "Invalid request method"
     }
 
     #foreach x [lsort [array names request]] {
-    #    _puts "$x = $request($x)"
+    #    logger "$x = $request($x)"
     #}
 }
 
 proc httpd::request {s addr req} {
     set_env $s $addr $req
-    if {[string match */files/* [dict get $req uri]]} {
+    if {[string match */files/* [dict get $req uri]] || [string match */include/* [dict get $req uri]]} {
         reply_file $s $addr $req
         return
     }
     if {[catch {service_request} out]} {
         if {$out != "exit"} {
-            if {[dict get $req method] == "POST"} { db eval {rollback transaction} }
-            httpd::http_error $s 500 "500 Internal Server Error" "error servicing request: $out<br>$::errorInfo"
+            logger "ERROR: $out"
+            if {[dict get $req method] == "POST"} { catch {db eval {rollback transaction}} }
+            http_error $s 500 "500 Internal Server Error" "error servicing request: $out<br>$::errorInfo"
         }
     }
 
     reply $s
 }
 
-proc reply_file {s addr req} {
+proc httpd::reply_file {s addr req} {
     set path [split [dict get $req uri] /]
     set i [lsearch $path files]
     set file $::request(DOCUMENT_ROOT)/[join [lrange $path $i end] /]
     if {![file exists $file]} {
-        httpd::http_error $s 404 "404 Not Found" "File not found"
+        http_error $s 404 "404 Not Found" "File not found"
     }
 
     _puts $s "HTTP/1.0 200 OK"
-    _puts $s "Content-Type: [httpd::mimetype $file]"
+    _puts $s "Content-Type: [mimetype $file]"
     _puts $s "Content-Length: [file size $file]\n"
 
     set fh [open $file r]
-    fconfigure $fh -encoding binary -translation lf
+    fconfigure $fh -encoding binary -translation lf -eofchar {}
     fconfigure $s -encoding binary -translation lf
     fcopy $fh $s
     close $fh
@@ -152,7 +153,7 @@ proc httpd::reply {s} {
             set reply_header [linsert $reply_header 0 "HTTP/1.0 200 OK"]
         }
     }
-    #_puts "reply_header: $reply_header"
+    #logger "reply_header: $reply_header"
     _puts $s [join [concat $reply_header $buf] \n]
     close $s
     unset buf
@@ -183,46 +184,109 @@ proc httpd::mimetype {file} {
     }
 }
 
-proc httpd::setup {cgi port} {
-    variable httpd
 
-    namespace eval :: [list source $cgi]
-    if {[catch {socket -server [list [namespace current]::incoming_connection] $port} socket]} {
+proc httpd::listen {port} {
+    variable state
+    variable config
+    if {[info exists state(socket)]} { catch {close $state(socket)} }
+
+    set cmd [list socket -server [list [namespace current]::incoming_connection]]
+    if {$config(local_only)} { lappend cmd -myaddr 127.0.0.1 }
+    lappend cmd $port
+    if {[catch {eval $cmd} socket]} {
         return -code error "cant open socket: $socket"
     }
-    set httpd(socket) $socket
-    set httpd(port) $port
+    set state(socket) $socket
+}
+
+proc httpd::setup {cgi} {
+    variable config
+    variable buf
+
+    fix_env
+    set config(base_dir) [pwd]
+    namespace eval :: [list source $cgi]
+    listen $config(port)
 
     rename ::exit ::_exit
-    proc ::exit {args} { return -code error exit }
-
     rename ::puts ::_puts
-    proc ::puts {args} {
-        #eval _puts $args
-        if {[lindex $args 0] == "-nonewline"} {
-            set args [lrange $args 1 end]
-        }
-        lappend httpd::buf [string trimright [join $args] \n]
-    }
-
-    close stdin
-    set chan [chan create read stdin]
+    rename ::read ::_read
+    namespace export puts exit read
 }
 
-proc ::stdin {args} {
-    #_puts "stdin: $args"
-    switch -exact -- [lindex $args 0] {
-        initialize { return {initialize finalize watch read} }
-        read {
-            set tmp [string range $::request(BODY) 0 [lindex $args 2]]
-            set ::request(BODY) [string range $::request(BODY) [lindex $args 2] end]
-            return $tmp
+proc httpd::exit {args} {
+    return -code error exit
+}
+
+proc httpd::read {chan args} {
+    if {$chan == "stdin"} {
+        return $::request(BODY)
+    }
+    return [eval [list _read $chan] $args]
+}
+
+proc httpd::puts {args} {
+    #eval logger $args
+    if {[lindex $args 0] == "-nonewline"} {
+        set args [lrange $args 1 end]
+    }
+    #lappend [namespace current]::buf [string trimright [join $args] \n]
+    lappend [namespace current]::buf [lindex $args 0]
+}
+
+proc httpd::logger {msg} {
+    set ts [clock format [clock seconds] -format {[%T]}]
+    _puts "$ts $msg"
+}
+
+proc httpd::fix_env {} {
+    global env
+    set keep [list REMOTE_ADDR HTTP_HOST PATH_INFO \
+        REQUEST_URI SERVER_PORT PATH_TRANSLATED \
+        DOCUMENT_ROOT QUERY_STRING SCRIPT_FILENAME \
+        REQUEST_METHOD CONTENT_TYPE HTTP_COOKIE]
+    foreach x [array names env] {
+        if {[lsearch -exact $keep $x] < 0} { unset env($x) }
+    }
+}
+
+proc httpd::parse_cmdline {in} {
+    variable config
+    foreach {opt arg} $in {
+        switch -exact -- $opt {
+            -port {
+                if {![string is integer -strict $arg]} { usage "port argument must be an integer" }
+                set config(port) $arg
+            }
+            -local_only {
+                if {![string is boolean -strict $arg]} { usage "port argument must be boolean" }
+                set config(local_only) $arg
+            }
+            default {
+                usage "unknown option $opt"
+            }
         }
     }
 }
 
-httpd::setup wiki.cgi 80
-# prefs, prefix, spawn browser
-set base_dir [pwd]
+proc httpd::usage {err} {
+    ::puts stderr "Error: $err\n"
+    ::puts stderr "Usage: $::argv0 \[-opt arg]"
+    ::puts stderr "    -port (Port to listen on for http connections) (default: 8080)"
+    ::puts stderr "    -local_only (Only allow connections from localhost) (default: true)"
+    ::exit 1
+}
 
-vwait forever
+# this will build the auto load index first, otherwise we cant import [read] later
+namespace import httpd::*
+
+namespace eval httpd {
+    variable state
+    array set config [list port 8080 local_only 1]
+    parse_cmdline $argv
+    setup wiki.cgi
+}
+
+namespace import httpd::*
+if {[catch {package present Tk}]} { vwait forever }
+if {[file exists httpd_gui.tcl]} { namespace eval httpd {source httpd_gui.tcl} }
