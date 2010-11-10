09611e88addb70a83b24495382a9f0fed90c029e
rox <rox>
2010-11-10 06:35:13 +0000

    fix path when running as starpack

--- crackwiki.vfs/main.tcl
+++ crackwiki.vfs/main.tcl
@@ -1,4 +1,7 @@
-  package require starkit
-  starkit::startup
-  set ::argv0 [file join $::argv0 lib crackwiki httpd.tcl]
-  package require crackwiki
+package require starkit
+starkit::startup
+if {[string match */main.tcl $::argv0]} {
+    set ::argv0 [file join {*}[lrange [file split $::argv0] 0 end-1]] 
+}
+set ::argv0 [file join $::argv0 lib crackwiki httpd.tcl]
+package require crackwiki
